03ec0a71820060f406fa480e3138ab74c701f409
mahranbhat65 <mahranbhat65@gmail.com>
2026-07-04 19:13:02 +0530

    added amf2.py

---
+++ amf2.py
@@ -0,0 +1,10 @@
+class Employee:
+    def __init__(self):
+
+        self.__salary = 50000
+    def show_salary(self):
+            
+            print(self.__salary)
+emp = Employee()
+emp.show_salary()
+
